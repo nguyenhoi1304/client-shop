0e3fe7eb42648b41e8dec8bdb395ec4bc737edb5
mixishop-ui <nguyenhoi13041999@gmail.com>
2023-12-12 05:08:18 +0700

    update api deploy

--- src/API/axiosClient.jsx
+++ src/API/axiosClient.jsx
@@ -5,7 +5,8 @@ import queryString from "query-string";
 // Please have a look at here `https://github.com/axios/axios#requestconfig` for the full list of configs
 const axiosClient = axios.create(
   {
-    baseURL: "http://localhost:5000",
+    baseURL: "https://backend-shop-rzlv.onrender.com",
+    // baseURL: "http://localhost:5000",
     headers: {
       "content-type": "application/json",
     },
